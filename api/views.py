c082797d99d77328226a3edcfa2df047f15c8b8e
Vish Vara <vishvara9@gmail.com>
2025-10-09 12:46:52 -0700

    updated file(s) in folder api

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Student
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
 from .serializers import StudentSerializers
 
@@ -12,8 +12,9 @@ def home(request):
 def student_detail(request, pk):
     stu = Student.objects.get(id=pk)
     serializers = StudentSerializers(stu)
-    json_data = JSONRenderer().render(serializers.data)
-    return HttpResponse(json_data, content_type = 'application/json')
+    # json_data = JSONRenderer().render(serializers.data)
+    # return HttpResponse(json_data, content_type = 'application/json')
+    return JsonResponse(serializers.data)
 
 def student_list(request):
     stu = Student.objects.all()
